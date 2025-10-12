2908b185ac1f0e0c4796ea78984e8d7fbb9c2b82
komal-kumawat <kumawatkomal17122004@gmail.com>
2025-10-12 15:13:37 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="bg-amber-500 text-4xl underline flex justify-around mb-20 py-10">
+        Hello
+      </div>
+      <div className="bg-blue-200 text-3xl flex justify-around py-10">
+        This is a react boiler-plate with typescript and tailwind
+      </div>
+    </>
+  );
+}
+
+export default App;
